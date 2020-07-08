ac1cc4c1adb9fda61a2e4dc3d6591456633a098b
pilarroma <pilar.rodriguez.martinnez@gmail.com>
2020-07-08 18:47:56 +0200

    Initial Commit

---
+++ send.php
@@ -0,0 +1,25 @@
+<?php 
+
+	$name = $_POST['name'];
+	$mail = $_POST['email'];
+	$empresa = $_POST['message'];
+
+	$header = 'De: ' . $mail . "\r\n";
+	$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+	$header .= "Mime-Version: 1.0 \r\n";
+	$header .= "Content-Type: text/plain";
+
+	$message = "Este mensaje fue enviado por " . $name , ",\r\n";
+	$message .= "Su e-mail es: " . $mail . "\r\n";
+	$message .= "Mensaje: " - $_POST['message'] . "\r\n";
+	$message .= "Enviado el " . date('d/m/Y', time());
+
+	$para = "pilar.rodriguez.martinez88@gmail.com";
+	$asunto = "Mensaje de la Web de Pilar Rodriguez Martinez";
+
+	mail($para, $asunto, utf8_decode($message), $header);
+	//Redirige a index despues de enviar el mensaje
+	header("Location:index.html");
+
+ ?>
+
