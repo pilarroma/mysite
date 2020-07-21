1c3cf04b66b213c696bd82a1b98984fe7daa6d98
Pilar Rodríguez Martínez <pilar.rodriguez.martinnez@gmail.com>
2020-07-21 12:51:30 +0200

    Update send.php
    Corrección de errores

--- send.php
+++ send.php
@@ -9,9 +9,9 @@
 	$header .= "Mime-Version: 1.0 \r\n";
 	$header .= "Content-Type: text/plain";
 
-	$message = "Este mensaje fue enviado por " . $name , ",\r\n";
+	$message = "Este mensaje fue enviado por " . $name . ",\r\n";
 	$message .= "Su e-mail es: " . $mail . "\r\n";
-	$message .= "Mensaje: " - $_POST['message'] . "\r\n";
+	$message .= "Mensaje: " . $_POST['message'] . "\r\n";
 	$message .= "Enviado el " . date('d/m/Y', time());
 
 	$para = "pilar.rodriguez.martinez88@gmail.com";
